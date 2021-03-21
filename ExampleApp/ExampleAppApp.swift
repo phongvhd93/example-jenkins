da73a4b83ea77eb17298878d4a3d3538da303e37
Phong Vo <phong.d@nimblehq.co>
2021-03-21 19:05:29 +0700

    Initial Commit

---
+++ ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Phong Vo on 21/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
